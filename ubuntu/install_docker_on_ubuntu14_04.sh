b372dfbf1a782dcafe723b60df0f8cf5145551c6
Kai Dev Hu <kdhu@thoughtworks.com>
2016-08-25 23:02:37 +0800

    [Kai] add new script for install mesos on ubuntu.

---
+++ ubuntu/install_docker_on_ubuntu14_04.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo rm /etc/apt/sources.list.d/docker.list
+sudo touch /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get purge lxc-docker
+apt-cache policy docker-engine
+sudo apt-get update
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y  docker-engine
+sudo service docker start
+sudo gpasswd -a $(whoami) docker
+sudo service docker restart
