caf9f30f676bd1113d324f7df20c8d6cd4b59b37
Kai Dev Hu <kdhu@thoughtworks.com>
2016-08-30 00:29:19 +0800

    optimise mesos installation script.

--- ubuntu/install_mesos_on_ubuntu.sh
+++ ubuntu/install_mesos_on_ubuntu.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+sudo echo "export JAVA_HOME=\"/usr/lib/jvm/java-8-oracle\"" | sudo tee -a /etc/environment
+source /etc/environment
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
